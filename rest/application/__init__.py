22c851e38ad8a9167da9ff040a76361c969186a1
mudro <vvgrygorchuk@gmail.com>
2024-02-11 22:46:16 +0200

    add configs for  mvc

--- rest/application/__init__.py
+++ rest/application/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_migrate import Migrate
+
 from application.question.questions_service import QuestionsService
 from application.answer.answer_service import AnswerService
+from application.models import db
+from application.answer.answer_controller import answer_bp
+from application.question.questions_controller import questions_bp
 
 
 app = Flask(__name__)
-questions_service  = QuestionsService()
-answers_service = AnswerService()
+app.config.from_object('config')
+db.init_app(app)
+migrate = Migrate(app, db)
+app.register_blueprint(answer_bp)
+app.register_blueprint(questions_bp)
 
-import application.question.questions_controller
-import application.answer.answer_controller
+# questions_service  = QuestionsService()
+# answers_service = AnswerService()
+
+# import application.question.questions_controller
+# import application.answer.answer_controller
--- rest/application/models.py
+++ rest/application/models.py
@@ -1,8 +1,21 @@
 from typing import TypeVar, Generic, List
-from dataclasses import dataclass
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
 
 T = TypeVar('T')
-class Page(Generic[T]):
+class Page(Generic[T], db.Model):
+    __tablename__ = 'pages'
+
+    id_col = db.Column(db.VARCHAR(12), primary_key=True)
+    size_col = db.Column(db.Int)
+    page_col = db.Column(db.Int)
+    total_pages_col = db.Column(db.Int)
+    content_col = db.Column(db.String(120))  
+
     def __init__(self, size: int, page: int, total_pages: int, content: List[T]):
         self.size = size
         self.page = page
@@ -20,5 +33,12 @@ class Page(Generic[T]):
             'content': [item.__dict__ for item in self.content]
         }
 
-    
-    
+    @property
+    def serialize(self):
+        return {
+            'id': Page.id_col,
+            'size': Page.size_col,
+            'page': Page.page_col,
+            'total_pages': Page.total_pages_col,
+            'content': Page.content_col
+        }
